34818cea81cf36d005fd592bf6cbac0167f42651
Divyaprakash Vellingiri <50037082+prakash1330@users.noreply.github.com>
2024-11-29 12:25:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
+
 # Adding env variable
 export IMAGE_NAME=$(cat info.txt)
 export TAG=$(cat tag.txt)
 
-# Stop the existing container
-docker compose down
-
 # Taking the container tage name from info.txt file
 docker build -t ${IMAGE_NAME}:${TAG} .
 
+# Stop the existing container
+docker-compose down
+
 # spin up a new container
-# Main
-docker compose up -d
+docker-compose up -d
 
 
 
