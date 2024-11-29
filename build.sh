ed5b95f617a1379b29130f68578c96df68b73043
Divyaprakash Vellingiri <50037082+prakash1330@users.noreply.github.com>
2024-11-29 15:21:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Adding env variable
-# export IMAGE_NAME=$(cat info.txt)
-# export TAG=$(cat tag.txt)
+export IMAGE_NAME=$(cat info.txt)
+export TAG=$(cat tag.txt)
 
 # Taking the container tage name from info.txt file
 docker build -t ${IMAGE_NAME}:${TAG} .
