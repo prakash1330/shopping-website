32d57bda6e337a6d57bdeacaa5ba821f02e7a5bf
Divyaprakash Vellingiri <50037082+prakash1330@users.noreply.github.com>
2024-11-29 12:44:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,13 +7,6 @@ export TAG=$(cat tag.txt)
 # Taking the container tage name from info.txt file
 docker build -t ${IMAGE_NAME}:${TAG} .
 
-# Stop the existing container
-docker-compose down
-
-
-# spin up a new container
-docker-compose up -d
-
 
 
 
