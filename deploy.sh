84dd1535ebd7eb811f9d1ff372d6763e74e2079e
Divyaprakash Vellingiri <50037082+prakash1330@users.noreply.github.com>
2024-11-29 14:26:02 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# setting up evn variable
+export IMAGE_NAME=$(cat info.txt)
+export TAG=$(cat tag.txt)
 
+# stopping the existing continer
 docker-compose down
 
+# spinning up new container
 docker-compose up -d
 
